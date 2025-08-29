f6cb98061b16506e6eeb5aa275853666ef47e6ae
Maxime Mauracheea <dev.mmaurach@gmail.com>
2025-08-29 11:48:15 +0900

    Ajout test NewBills fichier valide

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -6,6 +6,8 @@ import { screen } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -27,11 +29,15 @@ describe("Given I am connected as an employee", () => {
     );
     test("Must display an alert and reset the field", () => {
       window.alert = jest.fn();
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const store = null;
       const newBill = new NewBill({
-        document: document,
-        onNavigate: jest.fn(),
-        store: null,
-        localStorage: window.localStorage,
+        document,
+        onNavigate,
+        store,
+        localStorage: localStorageMock,
       });
       const fakeEvent = {
         preventDefault: jest.fn(),
@@ -45,6 +51,52 @@ describe("Given I am connected as an employee", () => {
         "Format de fichier invalide. Seuls les fichiers JPEG, JPG et PNG sont autorisés."
       );
       expect(fakeEvent.target.value).toBe("");
+    });
+  });
+  describe("When I am on NewBill Page and I upload a valid file", () => {
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+
+    test("Must NOT display an alert and must keep the field value", async () => {
+      window.alert = jest.fn();
+
+      const mockStore = {
+        bills: jest.fn(() => ({
+          create: jest.fn().mockResolvedValue({
+            fileUrl: "http://localhost/image.png",
+            key: "1234",
+          }),
+        })),
+      };
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: localStorageMock,
+      });
+
+      const fakeEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          value: "C:\\fakepath\\image.png",
+          files: [new File(["image"], "image.png", { type: "image/png" })],
+        },
+      };
+
+      await newBill.handleChangeFile(fakeEvent);
+
+      expect(window.alert).not.toHaveBeenCalled();
+      expect(fakeEvent.target.value).toBe("C:\\fakepath\\image.png");
     });
   });
 });
